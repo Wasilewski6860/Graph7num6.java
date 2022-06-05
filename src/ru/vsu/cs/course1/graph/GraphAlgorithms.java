d80b055b09a7e84c4d66f5729546782baa2a4346
Wasilewski6860 <91448408+Wasilewski6860@users.noreply.github.com>
2022-06-05 20:55:27 +0300

    Add files via upload

--- src/ru/vsu/cs/course1/graph/GraphAlgorithms.java
+++ src/ru/vsu/cs/course1/graph/GraphAlgorithms.java
@@ -27,37 +27,232 @@ public class GraphAlgorithms {
             }
         }
         new Inner().visit(from);
+
     }
 
-    public static void dfs(Graph graph, List<List<Integer>> result, List<Integer> path, int start,int finish,boolean[] visited){
-
+    public static void dfs(Graph graph, List<List<Integer>> result, List<Integer> path, int start, int finish, boolean[] visited) {
+        visited[start] = true;
         path.add(start);
 
-        if (start== finish ) {
-            result.add(new ArrayList(path));
+        if (start == finish) {
+            result.add(path);
         }
         else {
-            visited[start]=true;
+
+            List<Integer> tempPath = new ArrayList<>(path);
+            boolean[] tempVisited = visited.clone();
+
             for (int v : graph.adjacencies(start)) {
-                if (!visited[v]){
-                    visited[v]=true;
+
+                if (!visited[v]) {
                     dfs(graph, result, path, v, finish, visited);
-                    //         visited[v]=false;
+
+                    path = new ArrayList<>(tempPath);
+                    visited = tempVisited.clone();
+
+                }
             }
+        }
+    }
+
+    public static Graph listOfWaysToGraph(List<List<Integer>> listOfWays){
+        Graph graph = new AdjMatrixDigraph();
+        for (int i=0;i<listOfWays.size();i++){
+            for (int j=0;j<listOfWays.get(i).size()-1;j++){
+                if (!graph.isAdj(listOfWays.get(i).get(j),listOfWays.get(i).get(j+1)))
+                graph.addAdge(listOfWays.get(i).get(j),listOfWays.get(i).get(j+1));
             }
-            path.remove(path.size() - 1);
-            visited[path.size() - 1]=false;
+        }
+        return graph;
+    }
+
+
+    /**
+        Метод по пропускаю тока через граф, т.е. мы принимаем некоторую вершину from за источник тока(причем неважно, реальный ли это источник тока или нет),
+        и начинаем двигаться во все стороны, превращая неориентированные ребра в ориентированные,
+        что в дальнейшем позволит с помощью алгоритма поиска всех путей найти тот подграф, на котором мы будем искать сопротивление всей цепи
+
+         Пока что граф задается в виде матрицы инцидентности/смежности, с кодом Соломы совладать не могу
+
+         @param from - начало рассматриваемого участка
+         @param to  - конец рассматриваемого участка
+
+     Метод работает корректно(вроде, проверял несколько графов, пока проблемных случаев не выявлено), в исправлении не нуждается
+     */
+    public static boolean[][] bfsMy(boolean[][] graph, int from,int to) {
+
+        //  boolean[] visited = new boolean[graph.length];
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(from);
+        //     visited[from] = true;
+
+        //Заводим новый граф
+        boolean[][] newVisited = new boolean[graph.length][graph.length];
+        //Пока что граф несвязный, путей между вершинами нет
+        for (boolean[] lArr : newVisited){
+            for (boolean b : lArr){
+                b=false;
+            }
+        }
+
+        while (queue.size() > 0) {
+
+            Integer curr = queue.remove();
+            //Проходимся по всем вершинам, смежным с текущей
+            for(int i=0;i<graph[curr].length;i++){
+               //Если в новом графе еще нет пути из curr  в i и наоборот, а так же в исходном, неориентированном графе такой путь вообще есть,
+                // Так же рассматривается вариант, если мы уже выходили из вершины to в текущую,
+                // в таком случае возможно создание двустороннего ребра, которое после заменится на одно ребро в вершину to
+                if ((!newVisited[curr][i] && !newVisited[i][curr] && graph[curr][i]) ||(i==to) &&!newVisited[curr][i]&& graph[curr][i]) {
+                    queue.add(i);
+                    newVisited[curr][i]=true;
+                }
+            }
 
         }
+
+        //Исправление случая с двусторонним ребром в/из конечной вершины, заменяем на односторонее ребро
+        for (int i=0;i< newVisited[to].length;i++){
+            if (newVisited[i][to]) newVisited[to][i]=false;
+        }
+
+        return newVisited ;
+    }
+
+
+
+        //Метод по расчету сопротивления цепи
+    public static double resistance(boolean[][] adjMatrix,int[][] resistors,int curr,double R,boolean[] visited){
+
+        //Первое -- выясняем, последовательное ли соединение, или параллельное
+        // Для этого заводим счетчик смежных вершин
+        // Если есть путь между вершинами curr  и i,
+        // а так же мы еще не ходили в i(по идее, для орграфа мы не можем вернуться назад, но на всякий случай добавил и эту проверку)
+        // то счетчик наращивается
+        int count =0;
+        for (int i=0;i<adjMatrix[curr].length;i++){
+            if(adjMatrix[curr][i] && !visited[i]) count++;
+        }
+
+        //Если значение счетчика больше единицы, т.е. есть более двух смежных вершин, рассматриваем вариант параллельного соединения
+        if (count>1){
+
+            //Заводим переменнные под числитель и знаменатель
+            double Chislitel = 1;
+            double Znamenatel = 1;
+            //Проходимся по всем вершинам, смежным с текущей
+            for (int i=0;i< adjMatrix[curr].length;i++){
+            //Если путь есть, и мы еще не посещали вершину i, то
+                if (adjMatrix[curr][i] && !visited[i]){
+                    //Посещаем ее
+                    visited[curr]=true;
+                    //Проверки-вспомогашки
+                    System.out.println(" curRes:"+resistors[curr][i]+"  cur"+(curr+1)+" to"+(i+1)+"  R"+R);
+                    //Домножаем числитель на сопротивление текущего резистора, а так же рекурсивно вызываем функцию для i-й вершины
+                    //Аналогично со знаменателем, но там уже складываем
+                    Chislitel*=resistors[curr][i]*resistance(adjMatrix, resistors, i, R,visited);
+                    Znamenatel+=resistors[curr][i]+resistance(adjMatrix, resistors, i, R,visited);
+
+                }
+
+            }
+            //В итоге прибавляем к конечному сопротивлению цепи полученную дробь, как если бы это было последовательное соединение
+            R+=Chislitel/Znamenatel;
+            System.out.println("Chi"+Chislitel+" Zn"+Znamenatel);
+
+        }
+        //Случай последовательного соединения
+        //Проходимся по всем смежным вершинам(вообще-то она будет одна, и ее можно было бы найти заранее, но...)
+        else for (int i=0;i<adjMatrix[curr].length;i++){
+            //Если есть путь в i-ю вершину, и мы ее еще не посетили
+            if (adjMatrix[curr][i] && !visited[i]){
+                //Посещаем
+                visited[curr]=true;
+                System.out.println(" curRes:"+resistors[curr][i]+"  cur"+(curr+1)+" to"+(i+1)+"  R"+R);
+                //И попросту приплюсовываем к конечному сопротивлению сопр-е текущей ячейки + рекусивно вызываем для i-й вершины
+                R+=resistors[curr][i]+resistance(adjMatrix, resistors, i, R, visited);
+            }
+        }
+
+        return R;
+    }
+
+    //Проверяем на работоспособность
+    public static void testOfNormalWork(){
+
+        //Проверка на пропускание тока через граф
+        boolean[][] graph = {
+                {false,true,true,true,false,false,false,false,false},
+                {true,false,false,false,true,false,false,false,false},
+                {true,false,false,false,false,false,true,false,false},
+                {true,false,false,false,false,true,false,false,false},
+                {false,true,false,false,false,true,false,true,false},
+                {false,false,false,true,true,false,false,false,false},
+                {false,false,true,false,false,true,false,false,true},
+                {false,false,false,false,true,false,false,false,false},
+                {false,false,false,false,false,false,true,false,false}
+        };
+
+        graph= GraphAlgorithms.bfsMy(graph,0,5);
+        for (boolean[] bArr : graph){
+            System.out.println();
+            for (boolean b : bArr){
+                System.out.print(" "+b);
+            }
+        }
+
+
+        //Проверка на поиск сопртивления
+        boolean[][] newGraph = {
+                {false,true,true,true,false,false,false},
+                {false,false,false,false,true,false,false},
+                {false,false,false,false,false,false,true},
+                {false,false,false,false,false,true,false},
+                {false,false,false,false,false,true,false},
+                {false,false,false,false,true,false,true},
+                {false,false,false,false,false,true,false}
+        };
+        int[][] resistors = {
+                {0,5,9,6,0,0,0},
+                {0,0,0,0,6,0,0},
+                {0,0,0,0,0,0,6},
+                {0,0,0,0,0,21,0},
+                {0,0,0,0,0,8,0},
+                {0,0,0,0,8,0,1},
+                {0,0,0,0,0,1,0}
+        };
+
+        boolean[] visited = {false,false,false,false,false,false,false};
+        System.out.println(GraphAlgorithms.resistance(newGraph,resistors,0,0,visited));
     }
 
     public static List<List<Integer>> allPathSourceTarget(Graph graph,int start,int finish){
+
         boolean[] visited = new boolean[graph.vertexCount()];
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
         dfs(graph,result,path,start,finish, visited);
         return result;
+
     }
+
+    public static Set<Integer> commonVertices(List<List<Integer>> list){
+
+        Set<Integer> set = new HashSet<>();
+        set.addAll(list.get(0));
+
+        for (List<Integer> integerList : list){
+
+            Set<Integer> result = new HashSet<>();
+            result.addAll(integerList);
+
+           set.retainAll(result);
+
+        }
+
+        return set;
+    }
+
 
     /**
      * Поиск в глубину, реализованный с помощью стека
@@ -106,6 +301,7 @@ public class GraphAlgorithms {
             }
         }
     }
+
 
     /**
      * Поиск в глубину в виде итератора
--- src/ru/vsu/cs/course1/graph/demo/Program.java
+++ src/ru/vsu/cs/course1/graph/demo/Program.java
@@ -35,10 +35,14 @@ import static java.awt.Frame.MAXIMIZED_BOTH;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 
+import ru.vsu.cs.course1.graph.AdjMatrixDigraph;
+import ru.vsu.cs.course1.graph.GraphAlgorithms;
 import ru.vsu.cs.util.SwingUtils;
 
 public class Program {
@@ -50,22 +54,73 @@ public class Program {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
+
+        Set<Integer> result = new HashSet<Integer>();
+
+        Set<Integer> set1 = new HashSet<Integer>();
+
+        Set<Integer> set2 = new HashSet<Integer>();
+
+        set1.add(11);
+        set1.add(22);
+        set1.add(333);
+        set1.add(4);
+
+        set2.add(22);
+        set2.add(333);
+        set2.add(1);
+
+        System.out.println("set1 = "+set1.toString());
+        System.out.println("set2 = "+set2.toString());
+
+        result.clear();
+        result.addAll(set1);
+        result.retainAll(set2);
+        System.out.println ("Пересечение =" + result);
+
+        result.clear();
+        result.addAll(set1);
+        result.removeAll(set2);
+        System.out.println ("Набор различий =" + result);
+
+        result.clear();
+        result.addAll(set1);
+        result.addAll(set2);
+        System.out.println ("Союз =" + result);
+
+
+        AdjMatrixDigraph adjMatrixDigraph = new AdjMatrixDigraph();
+        adjMatrixDigraph.addAdge(1,2);
+        adjMatrixDigraph.addAdge(1,4);
+        adjMatrixDigraph.addAdge(1,5);
+        adjMatrixDigraph.addAdge(2,1);
+        adjMatrixDigraph.addAdge(4,1);
+        adjMatrixDigraph.addAdge(5,1);
+
+        adjMatrixDigraph.addAdge(8,4);
+        adjMatrixDigraph.addAdge(8,3);
+        adjMatrixDigraph.addAdge(5,1);
+        adjMatrixDigraph.addAdge(5,1);
+
         Locale.setDefault(Locale.ROOT);
+
+
+       GraphAlgorithms.testOfNormalWork();
 
         //SwingUtils.setLookAndFeelByName("Windows");
         //UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         //SwingUtils.setDefaultFont(null, 20);
-        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-        SwingUtils.setDefaultFont("Arial", 20);
-
-        EventQueue.invokeLater(() -> {
-            try {
-                JFrame mainFrame = new GraphDemoFrame();
-                mainFrame.setVisible(true);
-                mainFrame.setExtendedState(MAXIMIZED_BOTH);
-            } catch (Exception ex) {
-                SwingUtils.showErrorMessageBox(ex);
-            }
-        });
+//        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+//        SwingUtils.setDefaultFont("Arial", 20);
+//
+//        EventQueue.invokeLater(() -> {
+//            try {
+//                JFrame mainFrame = new GraphDemoFrame();
+//                mainFrame.setVisible(true);
+//                mainFrame.setExtendedState(MAXIMIZED_BOTH);
+//            } catch (Exception ex) {
+//                SwingUtils.showErrorMessageBox(ex);
+//            }
+//        });
     }
 }
